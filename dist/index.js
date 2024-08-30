6ce89d2ca5f67dea6a6d910cfb0a6ca54a25914a
sans-byte <sanskarj15@gmail.com>
2024-08-30 23:16:40 +0530

    middleware for analytics

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+let requestsCount = 0;
+//middleware for analytics
+app.use(function requestCount(req, res, next) {
+    requestsCount++;
+    next();
+});
+app.get("/", (req, res) => {
+    return res.json("Hello world");
+});
+app.get("/requestCount", (req, res) => {
+    return res.json(requestsCount);
+});
 app.listen(3000, () => {
     console.log("Connected");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import express from "express";
 const app = express();
 
+let requestsCount = 0;
+//middleware for analytics
+app.use(function requestCount(req, res, next) {
+  requestsCount++;
+  next();
+});
+
+app.get("/", (req, res) => {
+  return res.json("Hello world");
+});
+
+app.get("/requestCount", (req, res) => {
+  return res.json(requestsCount);
+});
+
 app.listen(3000, () => {
   console.log("Connected");
 });
